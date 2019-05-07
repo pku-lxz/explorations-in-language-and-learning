65f84108f44fe6a13f03cbc517e02b123ce131de
lamprad <1801210053@pku.edu.cn>
2019-05-07 12:39:28 +0800

    data processing done

--- common.py
+++ common.py
@@ -5,5 +5,8 @@ class config:
     data_dir = '/Users/lamprad/Desktop/cmn-eng/'
     model_save_dir = os.path.join(data_dir, 'tf_ckpt')
     tboard_save_dir = os.path.join(data_dir, 'tf_log')
-    train_size = 100
-    test_size = 70
+    train_size = 18000
+    test_size = 21007 - train_size
+    input_size = 50
+    train_batch_size = 64
+    test_batch_size = 64
--- dataset.py
+++ dataset.py
@@ -1,25 +1,81 @@
+#!/usr/bin/env python
+# coding:utf-8
 import os
 from common import config
 import re
+import random
+import numpy as np
+import pickle
 
-class Dataset:
-    def __init__(self, dataset_name='train'):
+
+class DataSet:
+    def __init__(self, dataset_name, read_from_pkl=True):
         assert dataset_name in ['train', 'test']
         self.ds_name = dataset_name
-        self.size = config.train_size if self.ds_name == 'train' else config.test_size
-        self.dataset, self.train, self.test = [], [], []
-
-    def loaddata(self):
-        file_name = os.path.join(config.data_dir, 'cmn.txt')
-        pattern = re.compile(r'[\.\?\!\"]')
-        with open(file_name) as f:
+        self.dataset = []
+        self.batch_size = config.train_batch_size if self.ds_name == 'train' else config.test_batch_size
+        with open(os.path.join(config.data_dir, 'cmn.txt'), 'r') as f:
             while True:
                 f_ = f.readline()
                 if f_:
-                    index = re.finditer(pattern, f_)
-                    for i_ in index:
-                        index_ = i_.span()
-                    self.dataset.append((f_[:index_[-1]], f_[index_[-1]:]))
+                    indexs = [m.start() for m in re.finditer(re.compile(r'[\u4e00-\u9fa5]'), f_)]
+                    self.dataset.append([f_[:indexs[0]].replace('\t', ''), f_[indexs[0]:].replace('\n', '')])
                 else:
                     break
+        random.shuffle(self.dataset)
+        self.train, self.test = self.dataset[:config.train_size], self.dataset[config.train_size:]
+        self.size = config.train_size if self.ds_name == 'train' else config.test_size
+        if read_from_pkl:
+            with open(os.path.join(config.data_dir, 'dictionary.pkl'), 'rb') as f:
+                self.dict = pickle.load(f)
+        else:
+            self.map_dic()
+            with open(os.path.join(config.data_dir, 'dictionary.pkl'), 'wb') as f:
+                pickle.dump(self.dict, f)
 
+    def map_dic(self):
+        """construct dictionary"""
+        chineseset, englishset = set(), set()
+        for bi_tuple in self.dataset:
+            for str_ in bi_tuple[0]:
+                englishset.add(str_)
+        englishdict = dict(zip(englishset, range(len(englishset))))
+        for bi_tuple in self.dataset:
+            for str_ in bi_tuple[1]:
+                chineseset.add(str_)
+        chinesedict = dict(zip(chineseset, range(len(chineseset))))
+        self.dict = dict(englishdict, **chinesedict)
+
+    @staticmethod
+    def raw_data_generator(data, dic):
+        X, y = [], []
+        for idx, sentence in enumerate(data):
+            sentence0, sentence1 = sentence[0], sentence[1]
+            X.append(np.array([dic[word] for word in sentence0]))
+            y.append(np.array([dic[word] for word in sentence1]))
+        return np.array(X), np.array(y)
+
+    def one_epoch_generator(self):
+        """生成一个epoch"""
+        idx = list(range(self.size))
+        if self.ds_name == "train":
+            X, y = self.raw_data_generator(self.train, self.dict)
+            np.random.shuffle(idx)
+            start = 0
+            while start < self.size:
+                end = start + self.batch_size
+                yield X[idx[start:end]], y[idx[start:end]]
+                start = end
+        else:
+            X, y = self.raw_data_generator(self.test, self.dict)
+            start = 0
+            while start < self.size:
+                end = start + self.batch_size
+                yield X[idx[start:end]], y[idx[start:end]]
+                start = end
+
+
+if __name__ == '__main__':
+    d = DataSet('train', read_from_pkl=True)
+    for i in d.one_epoch_generator():
+        print(i)
